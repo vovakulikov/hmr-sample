b59a3fd306634122957e241eb0d1200aa6f288e5
Vladimir Kulikov <vladimir.kulikov@team.wrike.com>
2020-08-11 18:50:47 +0300

    Add classic hot module reload example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hmr-sample';
+  counter = 1;
+
+  ngOnInit() {
+    console.log('hello init hook');
+  }
+
+  increment() {
+    this.counter = this.counter + 1;
+  }
+
+  decrement() {
+    this.counter = this.counter - 2;
+  }
 }
